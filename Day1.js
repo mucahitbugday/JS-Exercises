bca8a348b77b200dd69845fdc838b71c68160a88
Mücahit Buğday <mucahit.bugday@outlook.com>
2022-09-29 11:58:06 +0300

    Day1 ok

---
+++ Day1.js
@@ -0,0 +1,7 @@
+//typeof
+//Bir değişkenin veri türüni kontrol etmek için typeof operatörü kullanılır
+console.log(typeof "Ali")       //string
+console.log(typeof 5)           //numer
+console.log(typeof true)        //boolen   
+console.log(typeof null)        //opject type
+console.log(typeof undefined)   //undefined
